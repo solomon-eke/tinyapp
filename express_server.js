acac80e0599a168a022c8f808aba267110d64001
solomon-eke <ekesolomon11@gmail.com>
2021-11-09 01:21:09 +0000

    Add the express server file

---
+++ express_server.js
@@ -0,0 +1,17 @@
+const { response } = require("express");
+const express = require("express");
+const app = express();
+const PORT = 8080; // default port
+
+const urlDatabase = {
+  b2xVn2: "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com",
+};
+
+app.get("/", (request, response) => {
+  response.send("Hello!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
